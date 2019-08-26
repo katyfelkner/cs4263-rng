68817bb47519dce549540f8fcfd74706babf342e
Katy Felkner <katy.felkner@ou.edu>
2019-08-26 12:24:32 -0500

    basic version of client app

--- clientapps/clientapp.py
+++ clientapps/clientapp.py
@@ -1,1 +1,14 @@
 # python application to get one number and copy to clipboard
+
+import urllib
+
+appURL = "https://randompythongen.appspot.com/"
+# send post request to URL
+request = urllib.request.Request(appURL)
+
+with urllib.request.urlopen(request) as r:
+    # get response
+    print(f.read())
+    # parse response
+
+    # copy to clipboard
