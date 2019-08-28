c96823324dde024f0a2c9c891e0e5302c7523563
Lucas Bowker <43243085+lucasbwkr@users.noreply.github.com>
2019-08-28 12:44:25 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# Python Web App. Allows for get requests to generate a random number 1-1,000,000.
+from flask import Flask
+import random
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    x = random.randint(1,1000000)
+    return str(x)
+
+@app.route('/randomNumber/', methods=["GET"])
+def randomNumber():
+    x = random.randint(1,1000000)
+    return str(x)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
