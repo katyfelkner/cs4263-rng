2d3f8866ab0b71bba35ae052d4d6c8398f912bc6
JobVil <job@ou.edu>
2019-09-25 13:11:50 -0500

    creating java test test between regions

--- Testing/GetRequest.java
+++ Testing/GetRequest.java
@@ -4,45 +4,64 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 // Code based off of https://www.mkyong.com/webservices/jax-rs/restfull-java-client-with-java-net-url/
 public class GetRequest {
 
-    //http://java-random-number.appspot.com/demo
+
     public static void main(String[] args) {
+    	//Making a GET request to a domain and print respones 
+    	makeHTTPRequest("http://java-random-number.appspot.com/demo", "US-Central-1", "App", "Java");
+    	makeHTTPRequest("https://composed-anvil-254017.appspot.com/demo", "EU-WEST-1", "App", "Java");
+    	makeHTTPRequest("https://cs4263-test.appspot.com", "EU-WEST-1", "App", "Python");
+    	makeHTTPRequest("http://randompythongen.appspot.com/randomNumber/", "US-Central-1", "App", "Python");
+    	makeHTTPRequest("http://35.238.165.97:5000/", "EUS-Central-1", "Instance", "Python");
+    	makeHTTPRequest("http://35.238.165.97:5000/", "EU-WEST-1", "Instance", "Python");
+    	
+	
+    }
 
-	try {
+    public static void makeHTTPRequest(String urlS, String region, String domain, String lang) {
+    	try {
 
-	    URL url = new URL("http://java-random-number.appspot.com/demo");
-	    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-	    conn.setRequestMethod("GET");
-	    conn.setRequestProperty("Accept", "text/plane");
+		    URL url = new URL(urlS);
+		    long startTime = System.currentTimeMillis();
+		    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		    conn.setRequestMethod("GET");
+		    conn.setRequestProperty("Accept", "text/plane");
 
-	    if (conn.getResponseCode() != 200) {
-		throw new RuntimeException("Failed : HTTP error code : "
-					   + conn.getResponseCode());
-	    }
+		    if (conn.getResponseCode() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "
+						   + conn.getResponseCode());
+		    }
 
-	    BufferedReader br = new BufferedReader(new InputStreamReader(
-									 (conn.getInputStream())));
+		    BufferedReader br = new BufferedReader(new InputStreamReader(
+										 (conn.getInputStream())));
+			long elapsedTime = System.currentTimeMillis() - startTime;
+		    String output;
+		    InetAddress IP=InetAddress.getLocalHost();
+		    System.out.print(IP.getHostAddress()+": ");
+		    System.out.print(region + "_");
+		    System.out.print(domain + "_");
+		    System.out.print(lang + " @");
+		    System.out.print(elapsedTime+" ");
+		    while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		    }
+		    
+		    conn.disconnect();
 
-	    String output;
-	    System.out.println("Output from Server .... \n");
-	    while ((output = br.readLine()) != null) {
-		System.out.println(output);
-	    }
+		} catch (MalformedURLException e) {
 
-	    conn.disconnect();
+		    e.printStackTrace();
 
-	} catch (MalformedURLException e) {
+		} catch (IOException e) {
 
-	    e.printStackTrace();
+		    e.printStackTrace();
 
-	} catch (IOException e) {
-
-	    e.printStackTrace();
-
-	}
+		}
 
     }
 
