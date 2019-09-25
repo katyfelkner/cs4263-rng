ccaa0f98b14885b5a42c7673b962c5a62d6884f9
Alan Lee <43380253+alanlee47@users.noreply.github.com>
2019-09-25 11:29:15 -0500

    Updating testing script

--- Testing/testallinstances.py
+++ Testing/testallinstances.py
@@ -1,18 +1,16 @@
-# python application to test randoms and verify the 750/1000 requirement
-
 import urllib.request
-import time
+from time import perf_counter
 
-appURL = ["http://randompythongen.appspot.com/randomNumber/",
-        "http://35.238.165.97:5000/",
-        "http://java-random-number.appspot.com/demo"]
+appURL = ['http://randompythongen.appspot.com/randomNumber/',
+        'http://35.238.165.97:5000/',
+        'http://java-random-number.appspot.com/demo']
 
 for x in appURL:
     request = urllib.request.Request(x, method="GET")
-    t_0 = time.clock()
-    for i in range(0, 1000):
+    t_0 = perf_counter()
+    for i in range(0, 100):
         with urllib.request.urlopen(request) as r:
             response = r.read()
             number = int(response)#.decode("utf-8"))
-    print("Time for " + x + " is " + str(time.clock() - t_0))
-
+    t_f = perf_counter()
+    print('Time for ' + x + ' is ' + '{0:.3f}'.format(t_f - t_0) + ' seconds')
